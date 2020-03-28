aa2db0c6a2186535c9d49614b5d4bdcba21549bf
Larry Adams <ladams1776@yahoo.com>
2020-03-28 09:59:13 -0400

    going through and answering more of my questions

--- GasMileageMachineLearning/src/main/java/com/apache/spark/application/Main_DecisionTree.java
+++ GasMileageMachineLearning/src/main/java/com/apache/spark/application/Main_DecisionTree.java
@@ -14,7 +14,6 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataType;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
@@ -52,21 +51,30 @@ public class Main_DecisionTree {
     // Analyze the data
     analyzeData(cleanedIris, irisSchema);
     // Prepare for Machine Learning.
-
+    //@TODO: Stopped here
   }
 
+  /**
+   * Spark always has data stored in dataframes as strings, so we need to coerce them into the
+   * correct types we want to use. In this case, all of our feature variables should be doubles and
+   * our target variable is a string. So we are just converting here.
+   *
+   * @param iris
+   * @param irisSchema
+   * @return
+   */
   private static Dataset<Row> cleanseData(Dataset<Row> iris, StructType irisSchema) {
-
+    // Best for performance while converting. Only time we must play with a JavaRDD, is during the conversion of data types.
     final JavaRDD<Row> irisRepartitioned = iris.toJavaRDD().repartition(2);
 
-    final JavaRDD<Row> irisValuesCasted = irisRepartitioned.map(row ->
+    final JavaRDD<Row> valuesConverted = irisRepartitioned.map(row ->
         RowFactory.create(Double.valueOf(row.getString(0)),
             Double.valueOf(row.getString(1)),
             Double.valueOf(row.getString(2)),
             Double.valueOf(row.getString(3)),
             row.getString(4)));
 
-    final Dataset<Row> irisCleansed = sparkSession.createDataFrame(irisValuesCasted, irisSchema);
+    final Dataset<Row> irisCleansed = sparkSession.createDataFrame(valuesConverted, irisSchema);
 
     System.out.println("Cleaned Data (aka Transformed)");
     iris.show(5);
@@ -75,31 +83,16 @@ public class Main_DecisionTree {
   }
 
   /**
-   * <p>
    * We are choosing SPECIES as our 'target' variable - the variable we are going to compare other
    * variables (features) against, to figure out if we have correlations and how significant they
    * are.
-   * </p>
-   * <p>
-   * In order to make correlations against SPECIES field, we need to it to be in some sort of integer
-   * format. So we are going to use a StringIndexer, to map SPECIES to IND_SPECIES, which will be the
-   * indexing/referencing column we use, for correlation work.
-   * </p>
    *
-   * @param cleanedIris
-   * @param irisSchema
+   * @param cleanedIris the dataframe we cleaned and want to analyze
+   * @param irisSchema  the schema we can use to get the names for
    */
   private static void analyzeData(Dataset<Row> cleanedIris, StructType irisSchema) {
 
-    final StringIndexer indexer = new StringIndexer()
-        .setInputCol("SPECIES")
-        .setOutputCol("IND_SPECIES");
-
-    final StringIndexerModel siModel = indexer.fit(cleanedIris);
-    final Dataset<Row> indexedIris = siModel.transform(cleanedIris);
-    //@TODO: What are we checking here again? I believe we are grouping The species together to get a count,
-    //@TODO: Can't remember what the 0.0, 2.0, and 1.0 values are atm.
-    indexedIris.groupBy(col("SPECIES"), col("IND_SPECIES")).count().show();
+    final Dataset<Row> indexedIris = addAnIndexColumn(cleanedIris);
 
     System.out.println("Correlation between feature variable fields and the target variable field");
     // Perform Correlation Analysis
@@ -112,6 +105,29 @@ public class Main_DecisionTree {
 
           System.out.println(info.concat(String.valueOf(fieldsCorrelationToIndex)));
         });
+  }
+
+  /**
+   * In order to make correlations against SPECIES field, we need the field to be in a numeric form.
+   * So we are going to use a StringIndexer, to map SPECIES to IND_SPECIES, which will be the
+   * numeric index column we can use, for correlation work.
+   *
+   * @param cleanedIris the data frame we want the additional numeric index on
+   * @return the new data frame with the additional numeric index
+   */
+  private static Dataset<Row> addAnIndexColumn(Dataset<Row> cleanedIris) {
+    final StringIndexer indexer = new StringIndexer()
+        .setInputCol("SPECIES")
+        .setOutputCol("IND_SPECIES");
+
+    final StringIndexerModel siModel = indexer.fit(cleanedIris);
+    final Dataset<Row> indexedIris = siModel.transform(cleanedIris);
+
+    System.out
+        .println("The Species Column, it's numeric index value, and the amount of rows out there");
+    indexedIris.groupBy(col("SPECIES"), col("IND_SPECIES")).count().show();
+
+    return indexedIris;
   }
 
 }
